e59265d35ac92c8788727617b8c33dfaf238cba5
Asranov Baxtinur <asranov0003@gmail.com>
2023-09-08 12:06:28 +0500

    singleton

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 //Design Pattern
 // Creational Pattern - How objects are created.
-// -SINGLETON
-// -PROTOTYPE
-// -BUILDER
-// -FACTORY
+//  -SINGLETON
+//  -PROTOTYPE
+//  -BUILDER
+//  -FACTORY
 // Structural Pattern - How objects relate to each other.
-// -FACADE
-// -PROXY
+//  -FACADE
+//  -PROXY
 // Behavioral Pattern - How objects communicate with each other.
-// -ITERATOR
-// -OBSERVER
-// -MEDIATOR
-// -STATE
+//  -ITERATOR
+//  -OBSERVER
+//  -MEDIATOR
+//  -STATE
+//Creational Pattern
+//  1.Singleton
+var Singleton = /** @class */ (function () {
+    function Singleton() {
+    }
+    Singleton.getInstance = function () {
+        if (!Singleton.instance) {
+            Singleton.instance = new Singleton();
+        }
+        return Singleton.instance;
+    };
+    Singleton.prototype.showMessage = function () {
+        console.log("Hello from Singleton!");
+    };
+    return Singleton;
+}());
+var singleton1 = Singleton.getInstance();
+var singleton2 = Singleton.getInstance();
+singleton1.showMessage();
+console.log(singleton1 === singleton2);
--- script.ts
+++ script.ts
@@ -1,16 +1,41 @@
 //Design Pattern
 // Creational Pattern - How objects are created.
-    // -SINGLETON
-    // -PROTOTYPE
-    // -BUILDER
-    // -FACTORY
+//  -SINGLETON
+//  -PROTOTYPE
+//  -BUILDER
+//  -FACTORY
 
 // Structural Pattern - How objects relate to each other.
-    // -FACADE
-    // -PROXY
+//  -FACADE
+//  -PROXY
 
 // Behavioral Pattern - How objects communicate with each other.
-    // -ITERATOR
-    // -OBSERVER
-    // -MEDIATOR
-    // -STATE
+//  -ITERATOR
+//  -OBSERVER
+//  -MEDIATOR
+//  -STATE
+
+//Creational Pattern
+//  1.Singleton
+class Singleton {
+  private static instance: Singleton;
+
+  private constructor() { }
+
+  public static getInstance(): Singleton {
+    if (!Singleton.instance) {
+      Singleton.instance = new Singleton();
+    }
+    return Singleton.instance;
+  }
+
+  public showMessage(): void {
+    console.log("Hello from Singleton!");
+  }
+}
+
+const singleton1 = Singleton.getInstance();
+const singleton2 = Singleton.getInstance();
+
+singleton1.showMessage();
+console.log(singleton1 === singleton2); 
