a0c532e63ce4329ac0c14bfed64c739899061075
Asranov Baxtinur <asranov0003@gmail.com>
2023-09-08 11:56:32 +0500

    design pattern methods

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log(1);
+//Design Pattern
+// Creational Pattern - How objects are created.
+// -SINGLETON
+// -PROTOTYPE
+// -BUILDER
+// -FACTORY
+// Structural Pattern - How objects relate to each other.
+// -FACADE
+// -PROXY
+// Behavioral Pattern - How objects communicate with each other.
+// -ITERATOR
+// -OBSERVER
+// -MEDIATOR
+// -STATE
