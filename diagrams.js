b1285465b76bf973ae3f880f88589b88ac19a30d
Asranov Baxtinur <asranov0003@gmail.com>
2023-09-11 10:22:13 +0500

    diagrams

---
+++ diagrams.js
@@ -0,0 +1,17 @@
+class Dog {
+  constructor(color, height, length, weight, age) {
+    this.color = color;
+    this.height = height;
+    this.length = length;
+    this.weight = weight;
+    this.age = age;
+  }
+
+  sit() {}
+
+  bark() {}
+
+  run() {}
+
+  eath() {}
+}
