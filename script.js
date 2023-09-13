376d7adb796167c005b2f1558c5fc2adb09ae750
Asranov Baxtinur <asranov0003@gmail.com>
2023-09-13 10:25:00 +0500

    singleton pattern

--- script.js
+++ script.js
@@ -44,3 +44,28 @@ employees.push(employeeFactory.create("John", 2));
 employees.forEach(function (item) {
     say.call(item);
 });
+//Singleton Pattern
+function Process(state) {
+    this.state = state;
+}
+var Singleton = (function () {
+    function ProcessManager() {
+        this.numProcess = 0;
+    }
+    var pManager;
+    function createProcessManager() {
+        pManager = new ProcessManager();
+        return pManager;
+    }
+    return {
+        getProcessManager: function () {
+            if (!pManager) {
+                pManager = createProcessManager();
+                return pManager;
+            }
+        }
+    };
+})();
+var processManager = Singleton.getProcessManager();
+var processManager2 = Singleton.getProcessManager();
+console.log(processManager === processManager2);
--- script.ts
+++ script.ts
@@ -53,3 +53,35 @@ employees.push(employeeFactory.create("John", 2))
 employees.forEach((item) => {
   say.call(item)
 })
+
+//Singleton Pattern
+function Process(state) {
+  this.state = state;
+}
+
+const Singleton = (function () {
+  function ProcessManager() {
+    this.numProcess = 0
+  }
+
+  let pManager
+
+  function createProcessManager() {
+    pManager = new ProcessManager()
+    return pManager
+  }
+
+  return {
+    getProcessManager: () => {
+      if (!pManager) {
+        pManager = createProcessManager()
+        return pManager
+      }
+    }
+  }
+})()
+
+const processManager = Singleton.getProcessManager()
+const processManager2 = Singleton.getProcessManager()
+
+console.log(processManager === processManager2)
